868977a74e1976ce8ec7fe5a4deaa0a66019e3d4
Vishal Tyagi <visualtaggy@gmail.com>
2025-12-30 09:05:39 -0800

    feat: initial environment setup with i9 optimization

---
+++ env_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export PROJECT_ROOT=~/project-nebula
+export MAKEFLAGS="-j20"
+export CFLAGS="-O3 -march=native -pipe"
+export CXXFLAGS="${CFLAGS}"
+echo "Nebula Environment Loaded: i9 Ultra 185H Optimized."
